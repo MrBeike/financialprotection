bbdddb079451929211c82730b63e98b45d579fd6
aifly <aifly_xc@163.com>
2017-11-13 22:07:30 +0800

    a

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,9 +13,31 @@ var zmitiUtil = {
 
 		return isAndroid;
 	},
+
+	initComponent:function(){
+		Vue.component("zmiti-header",{
+			template:'<header>\
+			<h2></h2>\
+			<div class="zmiti-header-C">\
+				<div class="zmiti-menu-bar">\
+					<img src="./assets/images/menu-bar.png"/>\
+				</div>\
+				<div class="zmiti-city">\
+					<slot></slot>\
+				</div>\
+				<div class="zmiti-search-btn">\
+					<img src="./assets/images/search-btn.png" alt="">\
+				</div>\
+			</div>\
+		</header>'
+		});
+	},
+
 	init: function() {
 		var s = this;
 		var isAndroid = s.isAndroid();
+
+		s.initComponent();
 		this.vue = new Vue({
 			el: '#zmiti-main-ui',
 			data: {
@@ -54,8 +76,43 @@ var zmitiUtil = {
 
 		this.loadMapUI(map);
 
-	},
 
+		var mapObj = new AMap.Map('iCenter');
+		mapObj.plugin('AMap.Geolocation', function () {
+		    geolocation = new AMap.Geolocation({
+		        enableHighAccuracy: true,//是否使用高精度定位，默认:true
+		        timeout: 10000,          //超过10秒后停止定位，默认：无穷大
+		        maximumAge: 0,           //定位结果缓存0毫秒，默认：0
+		        convert: true,           //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+		        showButton: true,        //显示定位按钮，默认：true
+		        buttonPosition: 'LB',    //定位按钮停靠位置，默认：'LB'，左下角
+		        buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+		        showMarker: true,        //定位成功后在定位到的位置显示点标记，默认：true
+		        showCircle: true,        //定位成功后用圆圈表示定位精度范围，默认：true
+		        panToLocation: true,     //定位成功后将定位到的位置作为地图中心点，默认：true
+		        zoomToAccuracy:true      //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+		    });
+		    mapObj.addControl(geolocation);
+		    geolocation.getCurrentPosition();
+		    AMap.event.addListener(geolocation, 'complete', function(e){
+		    	console.log('success');
+		    	console.log(e);
+		    	$.ajax({
+		    		url:'http://api.zmiti.com/v2/msg/send_msg',
+		    		type:'post',
+		    		data:{
+		    			type:'xiaobao',
+		    			content:JSON.stringify(e),
+		    			to:''
+		    		}
+		    	})
+		    });//返回定位信息
+		    AMap.event.addListener(geolocation, 'error', function(){
+		    	console.log('error');
+		    });      //返回定位出错信息
+		});
+
+	},
 
 	loadMapUI: function(map) {
 
